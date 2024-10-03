0c5b0694968ef274f6a3987be7dd355c5ad77e10
Pedro Dias <pdr050203@gmail.com>
2024-10-03 18:21:49 -0300

    feat: add verification to execute one signal in different platforms

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native';
+import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { OneSignal } from 'react-native-onesignal'
@@ -11,8 +11,12 @@ import { Loading } from './src/components/Loading';
 import { CartContextProvider } from './src/contexts/CartContext';
 
 import { ONESIGNAL_ANDROID_FCM_ID } from '@env';
+import { ONESIGNAL_IOS_APNS_ID } from '@env';
 
-OneSignal.initialize(ONESIGNAL_ANDROID_FCM_ID)
+const oneSignalAppId = Platform.OS === "ios" ? 
+ONESIGNAL_IOS_APNS_ID : ONESIGNAL_ANDROID_FCM_ID
+
+OneSignal.initialize(oneSignalAppId)
 OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,3 +1,4 @@
 declare module '@env' {
   export const ONESIGNAL_ANDROID_FCM_ID: string;
+  export const ONESIGNAL_IOS_APNS_ID: string;
 }
