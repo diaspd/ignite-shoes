8417d7ec4f015ca1954ce2688d35cd8d8b5b4414
Pedro Dias <pdr050203@gmail.com>
2024-10-05 21:51:33 -0300

    feat: get notification info

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
 import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+
+import { OneSignal, type NotificationWillDisplayEvent } from 'react-native-onesignal';
 
 import { AppRoutes } from './app.routes';
 
@@ -8,6 +11,18 @@ export function Routes() {
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  useEffect(() => {
+    const handleNotification = (event: NotificationWillDisplayEvent): void => {
+      event?.preventDefault()
+      const response = event.getNotification()
+      console.log(response)
+    }
+
+    OneSignal.Notifications.addEventListener("foregroundWillDisplay", handleNotification)
+
+    return () => OneSignal.Notifications.removeEventListener("foregroundWillDisplay", handleNotification)
+  }, [])
 
   return (
     <NavigationContainer theme={theme}>
