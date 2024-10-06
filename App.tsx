18e96356708f507ce4963baa8750f195e6778285
Pedro Dias <pdr050203@gmail.com>
2024-10-06 00:08:46 -0300

    feat: add button action verification

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
+import { useEffect } from 'react';
 import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { OneSignal } from 'react-native-onesignal'
+import { OneSignal, type NotificationClickEvent } from 'react-native-onesignal'
 
 import { Routes } from './src/routes';
 
@@ -24,6 +25,32 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate()
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent) => {
+      const { actionId } = event.result
+
+      switch(actionId) {
+        case "1": 
+          console.log("Ver todos")
+          break
+        case "2": 
+          console.log("Ver pedido")
+          break
+        default: 
+          console.log("Nenhum botão de ação selecionado")
+          break
+      }
+    }
+
+    OneSignal.Notifications.addEventListener("click", handleNotificationClick)
+
+    return () => 
+      OneSignal.Notifications.removeEventListener(
+        "click",
+        handleNotificationClick
+      )
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
