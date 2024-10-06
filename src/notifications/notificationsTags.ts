1ba048b34d9bae09939efe5abd4f8090cea7dc7d
Pedro Dias <pdr050203@gmail.com>
2024-10-06 16:51:46 -0300

    feat: add tagCartUpdate function

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -2,7 +2,10 @@ import { OneSignal }from 'react-native-onesignal';
 
 export function tagUserInfoCreate() {
   OneSignal.User.addTags({
-    user_name: 'Pedro',
     user_email: 'pedro@email.com'
   });
+}
+
+export function tagCartUpdate(itemsCount: string) {
+  OneSignal.User.addTag("card_items_count", itemsCount)
 }
